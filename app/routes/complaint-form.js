93f107b60413d8202138138c541091c8222e3fb4
cecemel <ruiz.felixruiz@gmail.com>
2019-01-11 11:12:59 +0100

    Update complaint-form.js

--- app/routes/complaint-form.js
+++ app/routes/complaint-form.js
@@ -2,13 +2,16 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   async model() {
+    //felix
     const model = await this.store.createRecord('complaint-form');
+    this.set('form', model);
     return model;
   },
 
   actions: {
     async submitComplaint() {
-      let complaint = this.modelFor(this.routeName);
+      //felix
+      let complaint = this.form;
       var self = this;
       await complaint.save().then(function() {
         self.transitionTo('complaint-form');
