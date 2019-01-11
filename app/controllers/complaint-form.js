b054acf8aeced0f689d1ddc1073123b42fb93c13
Claire Lovisa <lovisa.claire@gmail.com>
2019-01-11 15:44:04 +0100

    Add validators to check the form fields

--- app/controllers/complaint-form.js
+++ app/controllers/complaint-form.js
@@ -1,7 +1,28 @@
 import Controller from '@ember/controller';
 import { task } from 'ember-concurrency';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+  showErrors: computed('_showErrors', {
+    get() {
+      return this._showErrors || {
+        name: false,
+        street: false,
+        houseNumber: false,
+        postalCode: false,
+        locality: false,
+        telephone: false,
+        email: false,
+        content: false
+      };
+    },
+
+    set(key, value) {
+      this.set('_showErrors', value);
+      return this._showErrors;
+    }
+  }),
+
   saveComplaintError: null,
 
   saveComplaint: task(function*() {
@@ -16,8 +37,8 @@ export default Controller.extend({
 
   actions: {
     submitComplaint() {
-      const res = this.saveComplaint.perform();
-      if(!this.saveComplaintError) {
+      this.saveComplaint.perform();
+      if (!this.saveComplaintError) {
         this.transitionToRoute('complaint-form');
       } else {
         // Display error message
--- app/models/complaint-form.js
+++ app/models/complaint-form.js
@@ -1,8 +1,83 @@
 import DS from 'ember-data';
+import { buildValidations, validator } from 'ember-cp-validations';
 
 const { Model, attr } = DS;
 
-export default Model.extend({
+const Validations = buildValidations({
+  name: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Naam is verplicht."
+    })
+  ],
+  street: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Straat is verplicht."
+    })
+  ],
+  houseNumber: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Huisnummer is verplicht."
+    }),
+    validator('number', {
+      allowString: true,
+      message: "Het veld Huisnummer moet een geldig Huisnummer bevatten"
+    })
+  ],
+  locality: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Gemeente of Stad is verplicht."
+    })
+  ],
+  postalCode: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Postcode is verplicht."
+    }),
+    validator('number', {
+      allowString: true,
+      gte: 1000,
+      lt: 10000,
+      message: "Het veld Postcode moet een geldig Postcode bevatten"
+    })
+  ],
+  telephone: [
+    validator('presence', false),
+    validator('number', {
+      allowBlank: true,
+      allowString: true,
+      message: "Het veld Telefoonnummer moet een geldig Telefoonnummer bevatten"
+    })
+  ],
+  email: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Mailadres is verplicht."
+    }),
+    validator('format', {
+      type: 'email',
+      message: 'Het veld Mailadres moet een geldig Mailadres bevatten'
+    })
+  ],
+  content: [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true,
+      message: "Het veld Omschrijving klacht is verplicht."
+    })
+  ],
+});
+
+export default Model.extend(Validations, {
   name: attr(),
   contactPersonName: attr(),
   street: attr(),
