2fd9eec1136f97f7b0b43fc832880ec2d68982db
Claire Lovisa <lovisa.claire@gmail.com>
2020-08-10 12:30:20 +0200

    Octanify routes

--- app/routes/complaint-form.js
+++ app/routes/complaint-form.js
@@ -1,7 +1,5 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
-@classic
 export default class ComplaintFormRoute extends Route {
   async model() {
     const model = await this.store.createRecord('complaint-form');
