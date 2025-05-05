5ead8202d2c449214d09a25ae5882c6ce9d49d9f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-05-05 17:05:23 +0200

    Update to Appuniversum v3

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -2,9 +2,4 @@
 
 module.exports = {
   generateTests: false,
-  allowedVersions: {
-    // Our dep tree includes both v3 and v4, which fails the lint but everything works fine.
-    // TODO: remove this once we update to Appuniversum v3
-    'ember-modifier': '*',
-  },
 };
