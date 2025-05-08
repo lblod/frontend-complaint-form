325ee306960c186eba58d378424c347b9d2b249d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-05-08 17:53:37 +0200

    Add a details page (wip)

--- app/routes/complaints/details.js
+++ app/routes/complaints/details.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class ComplaintsDetailsRoute extends Route {}
+export default class ComplaintsDetailsRoute extends Route {
+  @service store;
+
+  model(params) {
+    return {
+      complaintPromise: this.store.findRecord('complaint-form', params.id, { include: 'attachments' })
+    }
+  }
+}
