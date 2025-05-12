ef0100d8c552f82f3764acf81a5c01d5b09755f3
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-05-12 15:48:39 +0200

    Redirect download links to the complaints details page
    This ensures links in old mails keep working while still ensuring the user is logged in.

--- app/routes/file-redirect.js
+++ app/routes/file-redirect.js
@@ -1,3 +1,23 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class FileRedirectRoute extends Route {}
+export default class FileRedirectRoute extends Route {
+  @service router;
+  @service store;
+
+  async model(params) {
+    const results = await this.store.query('complaint-form', {
+      'filter[attachments][:id:]': params.id,
+    });
+
+    const linkedComplaintForm = results.at(0);
+
+    if (linkedComplaintForm) {
+      this.router.transitionTo('complaints.details', linkedComplaintForm.id);
+    } else {
+      throw new Error(
+        "Couldn't find the complaint form to which this file is attached.",
+      );
+    }
+  }
+}
